292173e2e27aff4d54fea75bf4f02b79bb17160a
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-05-24 23:04:52 -0700

    bug fixes

--- src/components/app.js
+++ src/components/app.js
@@ -6,27 +6,48 @@ import HeaderBar from './header-bar';
 import LandingPage from './landing-page';
 import Dashboard from './dashboard';
 import RegistrationPage from './registration-page';
-import {refreshAuthToken} from '../actions/auth';
+import {refreshAuthToken, clearAuth} from '../actions/auth';
+
 
 export class App extends React.Component {
     componentDidUpdate(prevProps) {
+
         if (!prevProps.loggedIn && this.props.loggedIn) {
             // When we are logged in, refresh the auth token periodically
+
             this.startPeriodicRefresh();
+
+            setTimeout(() => {
+                alert('Loging out soon')
+            }, 60 * 1000 * 4)
+
         } else if (prevProps.loggedIn && !this.props.loggedIn) {
             // Stop refreshing when we log out
             this.stopPeriodicRefresh();
         }
+
     }
 
-    componentWillUnmount() {
+
+    
+    componentDidMount() {
+
+
+        setTimeout(() => {
+            this.props.dispatch(clearAuth())
+        }, 60 * 1000 * 5)
+    }
+
+    componentWillUnmount() {//AutoLogout here?
         this.stopPeriodicRefresh();
+
     }
 
     startPeriodicRefresh() {
         this.refreshInterval = setInterval(
             () => this.props.dispatch(refreshAuthToken()),
-            60 * 60 * 1000 // One hour
+            // 60 * 60 * 1000 // One hour
+            10 * 60 * 1000 // Ten minutes
         );
     }
 
@@ -57,3 +78,4 @@ const mapStateToProps = state => ({
 
 // Deal with update blocking - https://reacttraining.com/react-router/web/guides/dealing-with-update-blocking
 export default withRouter(connect(mapStateToProps)(App));
+
--- src/store.js
+++ src/store.js
@@ -4,7 +4,8 @@ import thunk from 'redux-thunk';
 import {loadAuthToken} from './local-storage';
 import authReducer from './reducers/auth';
 import protectedDataReducer from './reducers/protected-data';
-import {setAuthToken, refreshAuthToken} from './actions/auth';
+// import {setAuthToken, refreshAuthToken} from './actions/auth';
+import {refreshAuthToken} from './actions/auth';
 
 const store = createStore(
     combineReducers({
@@ -19,7 +20,7 @@ const store = createStore(
 const authToken = loadAuthToken();
 if (authToken) {
     const token = authToken;
-    store.dispatch(setAuthToken(token));
+    // store.dispatch(setAuthToken(token));
     store.dispatch(refreshAuthToken());
 }
 
